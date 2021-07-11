6f18de442078825dda7c87d0a11bdbb8dd5cdc15
unknown <Roman Savchuk@dell-kip19.bmk.by>
2021-07-11 07:01:14 +0300

    modified js file

--- Timer/script.js
+++ Timer/script.js
@@ -1,14 +1,19 @@
+const timerButtons = document.getElementById('timer__buttons');
 const timerBtn = document.querySelectorAll('.timer__btn');
+const timerBtnFirst = document.querySelectorAll('.timer__btn-first');
 const settingBtn = document.querySelector('.header__btn');
 const modalWindow = document.querySelector('.setting-modal');
 const overlay = document.querySelector('.overlay');
 const modalWindowBtn = document.querySelector('.setting-modal__btn');
-const timerButtons = document.getElementById('timer__buttons');
+
 const timeEl = document.querySelector('.time');
 const timerButtonStart = document.querySelector('.timer__button-start');
 const timerButtonPause = document.querySelector('.timer__button-pause');
 const timerButtonProceed = document.querySelector('.timer__button-proceed');
-const valueFirst = document.querySelector('.value');
+const timerButtonStop = document.querySelector('.timer__button-stop');
+const valueTimeBig = document.querySelector('.value__time-big');
+const valueTimeMin = document.querySelector('.value__time-min');
+const valueTimeMid = document.querySelector('.value__time-mid');
 const messege = document.querySelector('.timer__messege');
 const settingInput = document.querySelectorAll('.parameters-item__input');
 const pauseBtn = document.querySelector('.pause');
@@ -16,12 +21,25 @@ let inputBigTime = document.querySelector('.input-big-time');
 let inputMinTime = document.querySelector('.input-min-time');
 let inputMidTime = document.querySelector('.input-mid-time');
 
-let countdown;
-let time = 0;
+let time = inputBigTime.value * 60;
+let clockRun = false;
+let type = "Work";
+let relaxTime;
+let relaxBigTime;
+let workTime;
 
-timerButtons.addEventListener('click', event => {
-   changeBtn(event.target);
+// modal window
+const closeModalWindow = () => {
+   modalWindow.classList.remove('active');
+}
+
+settingBtn.addEventListener('click', () => {
+   modalWindow.classList.add('active');
 })
+
+modalWindowBtn.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+// modal window
 
 const changeBtn = elem => {
    for (btn of timerBtn) {
@@ -30,16 +48,26 @@ const changeBtn = elem => {
    elem.classList.add('active');
 
    if (elem.getAttribute('value') === '2') {
+      valueTimeMin.style.display = 'block';
+      valueTimeBig.style.display = 'none';
+      valueTimeMid.style.display = 'none';
       colorScreenMinTime();
-      setTimeFirst(inputMinTime.value);
-      timer(inputMinTime.value * 60)
+      setMinTimeFirst(inputMinTime.value);
+      time = inputMinTime.value * 60;
    } else if (elem.getAttribute('value') === '3') {
+      valueTimeMid.style.display = 'block';
+      valueTimeBig.style.display = 'none';
+      valueTimeMin.style.display = 'none';
       colorScreenMiddleTime();
-      setTimeFirst(inputMidTime.value);
-      timer(inputMidTime.value * 60)
-   } else {
+      setMidTimeFirst(inputMidTime.value);
+      time = inputMidTime.value * 60;
+   } else if (elem.getAttribute('value') === '1') {
+      valueTimeBig.style.display = 'block';
+      valueTimeMid.style.display = 'none';
+      valueTimeMin.style.display = 'none';
       colorScreenBigTime();
-      setTimeFirst(inputBigTime.value);
+      setBigTimeFirst(inputBigTime.value);
+      time = inputBigTime.value * 60;
    }
 }
 
@@ -48,6 +76,9 @@ const colorScreenBigTime = () => {
    document.querySelector('.header__btn').style.background = '';
    document.querySelector('.timer__wpapper').style.background = '';
    timerButtonStart.style.color = '';
+   timerButtonStop.style.color = '';
+   timerButtonPause.style.color = '';
+   timerButtonProceed.style.color = '';
    messege.innerHTML = `Время работать!`;
    for (btn of timerBtn) {
       btn.style.background = '';
@@ -59,6 +90,9 @@ const colorScreenMinTime = () => {
    document.querySelector('.header__btn').style.background = 'rgb(123, 224, 238)';
    document.querySelector('.timer__wpapper').style.background = 'rgb(123, 224, 238)';
    timerButtonStart.style.color = 'rgb(59, 189, 206)';
+   timerButtonStop.style.color = 'rgb(59, 189, 206)';
+   timerButtonPause.style.color = 'rgb(59, 189, 206)';
+   timerButtonProceed.style.color = 'rgb(59, 189, 206)';
    messege.innerHTML = `Время перерыва!`;
    for (btn of timerBtn) {
       if (btn.classList.contains('active')) {
@@ -75,6 +109,9 @@ const colorScreenMiddleTime = () => {
    document.querySelector('.timer__wpapper').style.background = 'rgb(66, 146, 211)';
    document.querySelector('.timer__btn.active').style.background = 'rgb(41, 114, 173)';
    timerButtonStart.style.color = 'rgb(41, 114, 173)';
+   timerButtonStop.style.color = 'rgb(41, 114, 173)';
+   timerButtonPause.style.color = 'rgb(41, 114, 173)';
+   timerButtonProceed.style.color = 'rgb(41, 114, 173)';
    messege.innerHTML = `Время перерыва!`;
    for (btn of timerBtn) {
       if (btn.classList.contains('active')) {
@@ -85,8 +122,16 @@ const colorScreenMiddleTime = () => {
    }
 }
 
-const setTimeFirst = (value) => {
-   valueFirst.innerHTML = value;
+const setBigTimeFirst = (value) => {
+   valueTimeBig.innerHTML = value;
+}
+
+const setMinTimeFirst = (value) => {
+   valueTimeMin.innerHTML = value;
+}
+
+const setMidTimeFirst = (value) => {
+   valueTimeMid.innerHTML = value;
 }
 
 const setTime = (value) => {
@@ -94,78 +139,172 @@ const setTime = (value) => {
 }
 
 const changeInput = () => {
-   setTimeFirst(inputBigTime.value);
    inputBigTime.addEventListener('change', () => {
       let inputBigTimeVal = inputBigTime.value;
       inputBigTime.dataset.time = inputBigTimeVal * 60;
-      setTimeFirst(inputBigTimeVal);
-      timer(inputBigTime.dataset.time);
+      setBigTimeFirst(inputBigTimeVal);
    })
    inputMinTime.addEventListener('change', () => {
       let inputMinTimeVal = inputMinTime.value;
       inputMinTime.dataset.time = inputMinTimeVal * 60;
-      setTimeFirst(inputMinTimeVal);
-      timer(inputMinTime.dataset.time);
+      setMinTimeFirst(inputMinTimeVal);
    })
    inputMidTime.addEventListener('change', () => {
       let inputMidTimeVal = inputMidTime.value;
       inputMidTime.dataset.time = inputMidTimeVal * 60;
-      setTimeFirst(inputMidTimeVal);
-      timer(inputMidTime.dataset.time);
+      setMidTimeFirst(inputMidTimeVal);
    })
 }
 
-changeInput()
-
-const timer = (seconds) => {
-   timerButtonStart.addEventListener('click', () => {
-      timerButtonStart.style.display = 'none';
-      timerButtonPause.style.display = 'block';
-      clearInterval(countdown);
-      const now = Date.now();
-      const then = now + seconds * 1000;
-      decreaseTime(seconds);
-
-      countdown = setInterval(() => {
-         const seondsLeft = Math.round((then - Date.now()) / 1000);
-         if (seondsLeft < 0) {
-            clearInterval(countdown);
-            return;
-         }
-         decreaseTime(seondsLeft);
+const toggleClock = (reset) => {
+   if (reset) {
+      stopTimer();
+   } else if (clockRun === true) {
+      clockRun = false;
+      clearInterval(clockTimer);
+   } else {
+      clockRun = true;
+      clockTimer = setInterval(() => {
+         stepDown();
+         timer();
       }, 1000);
-   });
-   timerButtonPause.addEventListener('click', () => {
-      clearInterval(countdown);
-      timerButtonPause.style.display = 'none';
-      timerButtonStart.style.display = 'block';
-      timerButtonStart.innerHTML = `Продолжить`
-   });
+   }
+};
 
+const timer = () => {
+      // clearInterval(countdown);
+      // const now = Date.now();
+      // const then = now + seconds * 1000;
+      // decreaseTime(seconds);
+
+      // countdown = setInterval(() => {
+      //    const seondsLeft = Math.round((then - Date.now()) / 1000);
+      //    if (seondsLeft < 0) {
+      //       clearInterval(countdown);
+      //       return;
+      //    }
+      //    decreaseTime(seondsLeft);
+      // }, 1000);
+      // let result = "";
+      // let hours = Math.floor(time / 3600);
+      let minutes = Math.floor(time / 60);
+      let seconds = Math.floor(time % 60);
+      const display = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      // if (hours > 0) result += `${hours}:`;
+      // result += `${minutes}:${seconds}`;
+      // tracker.innerHTML = result;
+      setTime(display);
 }
 
-timer(inputBigTime.value * 60)
+// const decreaseTime = (seconds) => {
+//    const minutes = Math.floor(seconds / 60);
+//    const sec = seconds % 60;
+//    const display = `${minutes}:${sec < 10 ? '0' : ''}${sec}`;
 
-const decreaseTime = (seconds) => {
-   const minutes = Math.floor(seconds / 60);
-   const sec = seconds % 60;
-   const display = `${minutes}:${sec < 10 ? '0' : ''}${sec}`;
+//    setTime(display);
+// }
 
-   setTime(display);
-}
+const stopTimer = () => {
+   for (btn of timerBtn) {
+      if(btn.getAttribute('value') === '1') {
+         clearInterval(clockTimer);
+         time = workTime;
+         timer();
+      } else if(btn.getAttribute('value') === '2') {
+         clearInterval(clockTimer);
+         time = relaxTime;
+         timer();
+      } else {
+         clearInterval(clockTimer);
+         time = relaxBigTime;
+         timer();
+      }
+   }
+   // if (type === "Relax") {
+   //       clearInterval(clockTimer);
+   //       clockRun = false;
+   //       time = relaxTime;
+   //       // time = 0;
+   //       timer();
+   //  } else if(type === "RelaxBig") {
+   //       clearInterval(clockTimer);
+   //       clockRun = false;
+   //       time = relaxBigTime;
+   //       // time = 0;
+   //       timer();
+   //  } else {
+   //       clearInterval(clockTimer);
+   //       clockRun = false;
+   //       time = workTime;
+   //       // time = 0;
+   //       timer();
+   //  }
+};
 
-// modal window
-settingBtn.addEventListener('click', () => {
-   modalWindow.classList.add('active');
+const stepDown = () => {
+   if (time > 0) {
+     time--;
+   } else if (time === 0) {
+     if (type === "Work") {
+       type = "Relax";
+       time = inputMinTime.value * 60;
+     } else {
+       time = inputBigTime.value * 60;
+     }
+   }
+ };
+
+timerButtonStart.addEventListener('click', () => {
+   timerButtonStart.style.display = 'none';
+   timerButtonPause.style.display = 'block';
+   timerButtonStop.style.display = 'block';
+   
+   workTime = inputBigTime.value * 60;
+   relaxBigTime = inputMidTime.value * 60;
+   relaxTime = inputMinTime.value * 60;
+   
+   for(let btn of timerBtn) {
+      btn.addEventListener('click', () => {
+         if (confirm('Время таймера будет остановленно!') === true) {
+            timerButtonProceed.style.display = 'none';
+            timerButtonPause.style.display = 'none';
+            timerButtonStop.style.display = 'none';
+            timerButtonStart.style.display = 'block';
+            stopTimer();
+         } else {
+            stepDown();
+         }
+      })
+   }
+   toggleClock();
+});
+
+timerButtonPause.addEventListener('click', () => {
+   timerButtonPause.style.display = 'none';
+   timerButtonStop.style.display = 'block';
+   timerButtonProceed.style.display = 'block';
+   toggleClock();
+});
+
+timerButtonProceed.addEventListener('click', () => {
+   timerButtonProceed.style.display = 'none';
+   timerButtonPause.style.display = 'block';
+   toggleClock();
 })
 
-const closeModalWindow = () => {
-   modalWindow.classList.remove('active');
-}
+timerButtonStop.addEventListener('click', () => {
+   timerButtonProceed.style.display = 'none';
+   timerButtonPause.style.display = 'none';
+   timerButtonStop.style.display = 'none';
+   timerButtonStart.style.display = 'block';
+   toggleClock(true);
+})
 
-modalWindowBtn.addEventListener('click', closeModalWindow);
-overlay.addEventListener('click', closeModalWindow);
-// modal window
+timerButtons.addEventListener('click', event => {
+   changeBtn(event.target);
+});
 
-
-
+setBigTimeFirst(inputBigTime.value);
+changeInput();
