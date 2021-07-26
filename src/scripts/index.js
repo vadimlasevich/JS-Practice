e5e5a2db5710b6a82c7d1dff9712b070564939f7
vadimlasevich <lasevichv@mail.ru>
2021-07-26 18:18:29 +0300

    added base file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,12 @@ import "./calculator.js";
 import "./slider.js";
 import "./timer.js";
 import "./maze-game.js";
+import "./weather.js";
+
 
 import "../styles/calculator.scss";
 import "../styles/slider.scss";
 import "../styles/timer.scss";
 import "../styles/maze-game.scss";
+import "../styles/weather.scss";
 import "../styles/null.scss";
